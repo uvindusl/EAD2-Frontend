4fb8c52de4b096ae833b4ce55a8743459b0cd8c6
HarinduVihan <harinduvihan@gmail.com>
2025-03-06 23:23:00 +0530

    add address

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,7 +20,12 @@ function Footer() {
               garfields@gmail.com
             </a>
             <br></br>
+            <br></br>
             <p>+94 76-5506968</p>
+            <p>
+              Independence Arcade Commercial Complex, Independence Ave, Colombo
+              00700, Sri Lanka
+            </p>
           </div>
         </div>
         <div className="col">
@@ -28,21 +33,36 @@ function Footer() {
             <h4>Follow Us</h4>
           </div>
           <div className="row2">
+            <img
+              className="logo"
+              src="../src/assets/instagram.png"
+              alt="facebook"
+            />
             <a className="custom-a" href="#">
               Instagrame
             </a>
             <br></br>
+            <img
+              className="logo"
+              src="../src/assets/facebook.png"
+              alt="facebook"
+            />
             <a className="custom-a" href="#">
               Facebook
             </a>
             <br></br>
+            <img
+              className="logo"
+              src="../src/assets/twitter.png"
+              alt="facebook"
+            />
             <a className="custom-a" href="#">
               Twitter
             </a>
           </div>
         </div>
       </div>
-      <p>&copy; Garfield's Cheesy Haven</p>
+      <p>&copy; 2025 Garfield's Cheesy Haven</p>
     </footer>
   );
 }
