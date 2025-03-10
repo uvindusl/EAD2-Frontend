5efdbf78316b7b6bea86179f18181a471c0afff9
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-03-10 18:51:56 +0530

    Add OrderPage and OrderForm components for order processing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import FoodPage from "./pages/FoodPage";
 import "../src/css/App.css";
 import LoginPage from "./pages/LoginPage";
+import OrderPage from "./pages/OrderPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<LoginPage />}></Route>
           <Route path="/home" element={<HomePage />}></Route>
           <Route path="/food" element={<FoodPage />}></Route>
+          <Route path="/order" element={<OrderPage />}></Route>
         </Routes>
       </main>
     </div>
