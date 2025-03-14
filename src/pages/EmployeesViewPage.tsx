86569a749c0434b1d6e3dffa27e76633d0639649
HarinduVihan <harinduvihan@gmail.com>
2025-03-14 23:54:51 +0530

    create employee card (not dynamic)

---
+++ src/pages/EmployeesViewPage.tsx
@@ -0,0 +1,16 @@
+import EmployeeCard from "../components/EmployeeCard";
+//import "../css/EmployeesViewPage.css";
+
+function EmployeesViewPage() {
+  return (
+    <div>
+      <div className="employeeCard-container">
+        <div>
+          <EmployeeCard />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EmployeesViewPage;
