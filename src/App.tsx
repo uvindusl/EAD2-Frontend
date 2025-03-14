70848e8e7afa8b0d96ea8c4b6874fce100518651
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-03-15 00:27:53 +0530

    Remove OrderPage and OrderForm components from the application

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import HomePage from "./pages/HomePage";
 import FoodPage from "./pages/FoodPage";
 import "../src/css/App.css";
 import LoginPage from "./pages/LoginPage";
-import OrderPage from "./pages/OrderPage";
 import CartPage from "./pages/CartPage";
 import EmployeeLoginPage from "./pages/EmployeeLoginPage";
 import EmployeesViewPage from "./pages/EmployeesViewPage";
@@ -18,7 +17,6 @@ function App() {
           <Route path="/" element={<LoginPage />}></Route>
           <Route path="/home" element={<HomePage />}></Route>
           <Route path="/food/:id" element={<FoodPage />}></Route>
-          <Route path="/order" element={<OrderPage />}></Route>
           <Route path="/cart" element={<CartPage />}></Route>
           <Route path="/emplogin" element={<EmployeeLoginPage />}></Route>
           <Route path="/empview" element={<EmployeesViewPage />}></Route>
