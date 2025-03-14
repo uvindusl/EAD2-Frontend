e9118c278e7a54606b7b15046b9df726be21e278
HarinduVihan <harinduvihan@gmail.com>
2025-03-15 00:09:54 +0530

    create employee page (not dynamic)

--- src/pages/EmployeesViewPage.tsx
+++ src/pages/EmployeesViewPage.tsx
@@ -1,14 +1,18 @@
 import EmployeeCard from "../components/EmployeeCard";
 import "../css/EmployeeViewPage.css";
+import Footer from "../components/Footer";
+import NavBar from "../components/navBar";
 
 function EmployeesViewPage() {
   return (
     <div>
+      <NavBar />
       <div className="employeeCard-container">
         <div>
           <EmployeeCard />
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
