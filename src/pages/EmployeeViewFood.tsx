e1d0451a15f8a514db646a9a0c905be372bd34a5
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-03-14 17:11:41 +0530

    Add EmployeeViewFood page and FoodGrid component for displaying food items

---
+++ src/pages/EmployeeViewFood.tsx
@@ -0,0 +1,55 @@
+// FoodGridPage.tsx
+import React, { useState, useEffect } from "react";
+import FoodGrid from "../components/FoodGrid"; // Assuming FoodGrid.tsx is in the same directory or adjust the path
+import "../css/EmployeeViewFood.css"; // Create a CSS file for styling
+import axios from "axios";
+
+interface Food {
+  foodid: string;
+  foodname: string;
+  fooddescription: string;
+  foodimg: string;
+  qty: number;
+  price: number;
+}
+
+function EmployeeViewFood() {
+  const [foods, setFoods] = useState<Food[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const customerid = sessionStorage.getItem("customerId");
+  console.log("Customer ID: ", customerid);
+
+  useEffect(() => {
+    const apiUrl = "http://localhost:8081/food-micro/foods";
+
+    setLoading(true);
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        if (response.status === 200) {
+          setFoods(response.data);
+        } else if (response.status === 204) {
+          //HTTP 204 successfully connect with the server but no data returned
+          setFoods([]);
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching food data:", error);
+        setError("Failed to load food items. Please try again later.");
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="food-grid-page">
+      {foods.map((food) => (
+        <FoodGrid key={food.foodid} food={food} />
+      ))}
+    </div>
+  );
+}
+
+export default EmployeeViewFood;
