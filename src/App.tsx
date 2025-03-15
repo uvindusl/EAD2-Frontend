3014b9673897a1eb3b844955a9d53fe936397baf
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-03-15 23:04:25 +0530

    Update routing in App component to include new employee and checkout pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import HomePage from "./pages/HomePage";
 import FoodPage from "./pages/FoodPage";
 import "../src/css/App.css";
 import LoginPage from "./pages/LoginPage";
-import OrderPage from "./pages/OrderPage";
 import CartPage from "./pages/CartPage";
 import EmployeeLoginPage from "./pages/EmployeeLoginPage";
-import AddFood from "./pages/AddFood";
+import EmployeesViewPage from "./pages/EmployeesViewPage";
+import EmployeeViewFood from "./pages/EmployeeViewFood";
+import CheckoutPage from "./pages/CheckoutPage";
+import AddEmployeePage from "./pages/AddEmployeePage";
+import UpdateEmployee from "./components/UpdateEmployee";
 
 function App() {
   return (
@@ -16,10 +19,13 @@ function App() {
           <Route path="/" element={<LoginPage />}></Route>
           <Route path="/home" element={<HomePage />}></Route>
           <Route path="/food/:id" element={<FoodPage />}></Route>
-          <Route path="/order" element={<OrderPage />}></Route>
           <Route path="/cart" element={<CartPage />}></Route>
           <Route path="/emplogin" element={<EmployeeLoginPage />}></Route>
-          <Route path="/addfood" element={<AddFood />}></Route>
+          <Route path="/empview" element={<EmployeesViewPage />}></Route>
+          <Route path="/foodview" element={<EmployeeViewFood />}></Route>
+          <Route path="/checkout" element={<CheckoutPage />}></Route>
+          <Route path="/addemployee" element={<AddEmployeePage />}></Route>
+          <Route path="/updateemployee/:id" element={<UpdateEmployee />} />
         </Routes>
       </main>
     </div>
