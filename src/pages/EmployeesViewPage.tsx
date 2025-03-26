b4518af0e5a6a12e54f296f7fc50b8af1d2e1353
HarinduVihan <harinduvihan@gmail.com>
2025-03-26 21:50:09 +0530

    food add text-area css

--- src/pages/EmployeesViewPage.tsx
+++ src/pages/EmployeesViewPage.tsx
@@ -77,7 +77,7 @@ function EmployeesViewPage() {
           </div>
         )}
       </div>
-      <div className="footer">
+      <div className="footer6">
         <Footer />
       </div>
     </div>
